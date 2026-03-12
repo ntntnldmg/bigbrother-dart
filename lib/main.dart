7f8bed0cccc33f12f4df80ba47e925fb052d2da8
Mussie Alemayehu <157012749+mussie-alemayehu@users.noreply.github.com>
2026-03-12 19:45:00 +0300

    refactor main intro and game screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'game/game_cubit.dart';
-import 'ui/intro_screen.dart';
+import 'screens/intro_screen.dart';
 
 void main() {
   runApp(const BigBrotherApp());
